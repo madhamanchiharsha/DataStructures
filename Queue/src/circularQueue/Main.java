8ef0932a06d14a0342f9b587d80981e6c569a77f
Harsha <harshavardhan.madhamanchi@philips.com>
2023-06-30 00:47:31 +0530

    added queue but need to add peek for circular and linear queue

---
+++ Queue/src/circularQueue/Main.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package circularQueue;
+
+import linearQueue.model.SingleLinkedListNode;
+
+/**
+ * @author 320069988
+ *
+ */
+public class Main {
+
+	SingleLinkedListNode<Integer> front = null;
+	SingleLinkedListNode<Integer> rear = null;
+	
+	private void enqueue(int data) {
+		// TODO Auto-generated method stub
+		if(this.front == null && this.rear == null) {
+			this.rear = new SingleLinkedListNode<Integer>(data,null);
+			this.rear.setNext(rear);
+			this.front = this.rear;
+			return;
+		}
+		
+		SingleLinkedListNode<Integer> temp = this.front;
+		SingleLinkedListNode<Integer> newNode = new SingleLinkedListNode<Integer>(data,null);
+		while(temp.getNext() != this.front) {
+			temp = temp.getNext();
+		}
+		temp.setNext(newNode);
+		newNode.setNext(this.front);
+		this.rear = newNode;
+	}
+	
+	private void dequeue() {
+		// TODO Auto-generated method stub
+		if(this.front == null && this.rear == null) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		SingleLinkedListNode<Integer> temp = this.front;
+		SingleLinkedListNode<Integer> prev = null;
+		while(temp.getNext() != this.front) {
+			prev = temp;
+			temp = temp.getNext();
+		} 
+		if(prev == null || this.front.getNext() == null) {
+			this.front = null;
+			this.rear = null;
+			return;
+		}
+		this.front = this.front.getNext();
+		temp.setNext(this.front);
+	}
+	
+	private void display() {
+		// TODO Auto-generated method stub
+		if(this.front == null && this.rear == null) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		SingleLinkedListNode<Integer> temp = this.front;
+		while(temp.getNext() != this.front) {
+			System.out.print(temp.getData()+" -> ");
+			temp = temp.getNext();
+		}
+		System.out.println(temp.getData());
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main obj = new Main();
+		obj.enqueue(0);
+		obj.enqueue(1);
+		obj.enqueue(2);
+		obj.enqueue(3);
+		obj.enqueue(4);
+		obj.display();
+		obj.dequeue();
+		obj.dequeue();
+		obj.dequeue();
+		obj.dequeue();
+		obj.dequeue();
+		obj.display();
+
+	}
+
+}
---
+++ Queue/src/linearQueue/Main.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package linearQueue;
+
+import linearQueue.model.SingleLinkedListNode;
+
+/**
+ * @author 320069988
+ *
+ */
+public class Main {
+
+	SingleLinkedListNode<Integer> front = null;
+	SingleLinkedListNode<Integer> rear = null;
+	
+	private void enqueue(int data) {
+		// TODO Auto-generated method stub
+		if(this.rear == null && this.front == null) { 
+			this.rear = new SingleLinkedListNode<Integer>(data, null);
+			this.front = this.rear;
+			return;
+		}
+		SingleLinkedListNode<Integer> temp = this.front;
+		SingleLinkedListNode<Integer> newNode = new SingleLinkedListNode<Integer>(data,null);
+		while(temp.getNext() != null) {
+			temp = temp.getNext();
+		}
+		temp.setNext(newNode);
+		this.rear = newNode;
+	}
+	
+	private void display() {
+		// TODO Auto-generated method stub
+		if( this.rear ==null && this.front == null) { 
+			System.out.println("Queue is empty");
+			return;
+		}
+		SingleLinkedListNode<Integer> temp = this.front;
+		while(temp.getNext() != null) {
+			System.out.print(temp.getData()+" -> ");
+			temp = temp.getNext();
+		}
+		System.out.println(temp.getData());
+	}
+	
+	private void dequeue() {
+		// TODO Auto-generated method stub
+		if(this.rear == null && this.front == null) { 
+			System.out.println("Rear and front both are empty");
+			return;
+		}
+		this.front = this.front.getNext();
+		if(this.front == null) {
+			this.rear = null;
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main obj = new Main();
+		obj.enqueue(0);
+		obj.enqueue(1);
+		obj.enqueue(2);
+		obj.enqueue(3);
+		obj.display();
+		obj.dequeue();
+		obj.dequeue();
+		obj.dequeue();
+		obj.dequeue();
+		obj.display();
+	}
+
+}
